cee19a2ca66d6a8b36c74566ace352d7f6a59ded
AlexCMTU <alexcros@mtu.edu>
2026-02-11 16:25:23 +0000

    feat: Added logger

--- Test.java
+++ Test.java
@@ -1,5 +1,8 @@
 public class Test {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    Test.log("Hello World");
   }
+
+  public static void log(String msg) {
+    System.out.println(msg);
 }
